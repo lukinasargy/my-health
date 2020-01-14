7bd004178d80ba04e886dd6abfdccf559b1f8f95
Sargy <lukina_sargy@mail.ru>
2020-01-14 18:12:19 +0300

    validation script replaced

--- app/js/common.js
+++ app/js/common.js
@@ -22,5 +22,21 @@ $(function () {
 		}, 1000);
 	});
 
+$('.order__btn').on('click', function(event, mail) {
+			event.preventDefault();
+			ValidateEmail(document.emailForm.emailFormInput);
+		});
 
+		function ValidateEmail(inputText) {
+			var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+			if (inputText.value.match(mailformat)) {
+				document.emailForm.emailFormInput.focus();
+				$(".order__info").removeClass('order__info--invalid').addClass('order__info--valid').fadeIn(300).html('Письмо отправлено на вашу почту');
+				return true;
+			} else {
+				document.emailForm.emailFormInput.focus();
+				$(".order__info").removeClass('order__info--valid').addClass('order__info--invalid').fadeIn(300).html('Вы ввели неправильный e-mail адрес');
+				return false;
+			}
+		}
 });
